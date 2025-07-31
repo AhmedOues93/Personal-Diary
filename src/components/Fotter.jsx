7914499a93532c0d910e4aa0f53f7f4ebdeca624
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-07-31 09:33:10 +0200

    footer

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -5,6 +5,7 @@ const Fotter = () => {
 <footer className="footer sm:footer-horizontal footer-center bg-black/40 text-base-content p-4">
   <aside>
     <p>Copyright © {new Date().getFullYear()} - All right reserved by Ahmed Oueslati</p>
+    <p> WBS coding school </p>
   </aside>
 </footer>
   )
